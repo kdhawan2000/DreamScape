6e8e211eadf801af5c5cc0fe1d82914ebedfc7f2
Sumanyu Kumar Arya <mesuarya28@gmail.com>
2024-04-10 08:27:54 -0600

    Hardcoded Calendar Event because it won't run locally

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -25,25 +25,34 @@ function generateTimeSlots(currentDate) { // Pass currentDate as a parameter
 
         const currentTimeString = `${String(i).padStart(2, '0')}:00`;
 
-        fetch('../user-database/events.json')
-            .then(response => response.json())
-            .then(data => {
-                const currentDateISOString = currentDate.toISOString().split('T')[0];
-                const event = data.find(event => event.date === currentDateISOString && event.start_time === currentTimeString);
+        // fetch('../user-database/events.json')
+        //     .then(response => response.json())
+        //     .then(data => {
+        //         const currentDateISOString = currentDate.toISOString().split('T')[0];
+        //         const event = data.find(event => event.date === currentDateISOString && event.start_time === currentTimeString);
 
-                if (event) {
-                    let button = document.createElement("button");
-                    button.innerText = event.name;
-                    button.classList.add("event-button");
+        //         if (event) {
+        //             let button = document.createElement("button");
+        //             button.innerText = event.name;
+        //             button.classList.add("event-button");
 
-                    button.addEventListener('click', () => {
-                        window.location.href = "../views/eventDetailsPageBrunoMars2.html";
-                    });
+        //             button.addEventListener('click', () => {
+        //                 window.location.href = "../views/eventDetailsPageBrunoMars2.html";
+        //             });
 
-                    eventCell.appendChild(button);
-                }
-            })
-            .catch(error => console.error('Error:', error));
+        //             eventCell.appendChild(button);
+        //         }
+        //     })
+        //     .catch(error => console.error('Error:', error));
+        if (currentDate.toISOString().split('T')[0] === '2024-04-22' && i === 19) {
+            let button = document.createElement("button");
+            button.innerText = "Bruno Mars Eras Tour";
+            button.classList.add("event-button");
+            button.addEventListener('click', () => {
+                window.location.href = "../views/eventDetailsPageBrunoMars2.html";
+            });
+            eventCell.appendChild(button);
+        }
 
         row.appendChild(timeSlotCell);
         row.appendChild(eventCell);
